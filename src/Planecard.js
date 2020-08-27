dd3c3c20ee65fdc46709b4631f78cc2a5e396868
MattLong34 <mdlong34@gmail.com>
2020-08-27 09:38:26 -0600

    correctly render all cards

--- src/Planecard.js
+++ src/Planecard.js
@@ -2,35 +2,16 @@ import React, {Component} from 'react'
 // import React from 'react';
 import './Planecard.css'
 
-import jupiterImage from './img/012-jupiter.svg'
-import marsImage from './img/017-mars.svg'
-import mercuryImage from './img/018-mercury.svg'
-import neptuneImage from './img/022-neptune.svg'
-import saturnImage from './img/028-saturn.svg'
-import uranusImage from './img/035-uranus.svg'
-import venusImage from './img/036-venus.svg'
-import plutoImage from './img/011-planet.svg'
-
 class Planecard extends Component {
-    static defaultProps = {
-        planet: [
-            {id: 1, name: 'Mercury', type: 'terrestrial'},
-            {id: 2, name: 'Venus', type: 'terrestrial'},
-            {id: 3, name: 'Earth', type: 'terrestrial'},
-            {id: 4, name: 'Mars', type: 'terrestrial'},
-            {id: 5, name: 'Jupiter', type: 'jovial'},
-            {id: 6, name: 'Saturn', type: 'jovial'},
-            {id: 7, name: 'Uranus', type: 'jovial'},
-            {id: 8, name: 'Neptune', type: 'jovial'},
-            {id: 9, name: 'Pluto', type: 'dwarf'},
-        ]
-    }
+    
     render(){
         return (
             <div className="Planecard">
                 <h1 className="Planecard-title">{this.props.name}</h1>
-                <img src={jupiterImage} alt={this.props.name} />
-                <p className="Planecard-data">A {this.props.type} planet</p>
+                <div>
+                    <img src={this.props.image} alt={this.props.name} />
+                    <p className="Planecard-data">A {this.props.type} planet</p>
+                </div>
             </div>
         )
     }
--- src/Planedex.js
+++ src/Planedex.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react'
 import Planecard from './Planecard'
 import './Planedex.css'
 
+import jupiterImage from './img/012-jupiter.svg'
+import marsImage from './img/017-mars.svg'
+import mercuryImage from './img/018-mercury.svg'
+import earthImage from './img/006-planet-earth.svg'
+import neptuneImage from './img/022-neptune.svg'
+import saturnImage from './img/028-saturn.svg'
+import uranusImage from './img/035-uranus.svg'
+import venusImage from './img/036-venus.svg'
+import plutoImage from './img/011-planet.svg'
+
 class Planedex extends Component{
     static defaultProps = {
         planet: [
-            {id: 1, name: 'Mercury', type: 'terrestrial'},
-            {id: 2, name: 'Venus', type: 'terrestrial'},
-            {id: 3, name: 'Earth', type: 'terrestrial'},
-            {id: 4, name: 'Mars', type: 'terrestrial'},
-            {id: 5, name: 'Jupiter', type: 'jovial'},
-            {id: 6, name: 'Saturn', type: 'jovial'},
-            {id: 7, name: 'Uranus', type: 'jovial'},
-            {id: 8, name: 'Neptune', type: 'jovial'},
-            {id: 9, name: 'Pluto', type: 'dwarf'},
+            {id: 1, name: 'Mercury', type: 'terrestrial', image: mercuryImage},
+            {id: 2, name: 'Venus', type: 'terrestrial', image: venusImage},
+            {id: 3, name: 'Earth', type: 'terrestrial', image: earthImage},
+            {id: 4, name: 'Mars', type: 'terrestrial', image: marsImage},
+            {id: 5, name: 'Jupiter', type: 'jovial', image: jupiterImage},
+            {id: 6, name: 'Saturn', type: 'jovial', image: saturnImage},
+            {id: 7, name: 'Uranus', type: 'jovial', image: uranusImage},
+            {id: 8, name: 'Neptune', type: 'jovial', image: neptuneImage},
+            {id: 9, name: 'Pluto', type: 'dwarf', image: plutoImage},
         ]
     }
     render(){
@@ -22,10 +32,10 @@ class Planedex extends Component{
                 <h1>Planedex!</h1>
                 <div className="Planedex-cards">
                     {this.props.planet.map((planet) => (
-                        <Planecard id={planet.id} name={planet.name} type={planet.type} />
+                        <Planecard id={planet.id} name={planet.name} type={planet.type} image={planet.image} />
                     ))}
                 </div>
-        </div>
+            </div>
         )
     }
 }
